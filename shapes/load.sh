51ffaa314c7f1bc5ee238318ffa35137231d0ca6
Grahame Bowland <grahame@oreamnos.com.au>
2018-08-02 18:57:48 +1000

    reproducible data load

--- shapes/load.sh
+++ shapes/load.sh
@@ -1,8 +1,21 @@
+#!/bin/bash -x
+
+loadshape()
+{
+  f="$PWD/$1"
+  srid="$2"
+  tempd=$(mktemp -d)
+  (cd "$tempd" && unzip "$f" && shp2pgsql -s "$srid" *.shp | psql -U postgres perth && rm -rf "$tempd")
+}
+
+apt-get update && apt-get -y install unzip
+
 dropdb -U postgres perth
 createdb -U postgres perth
 echo 'CREATE EXTENSION postgis;' | psql -U postgres perth
-shp2pgsql -s 3857 IntersectionsMRWA_510.shp | psql -U postgres perth
-shp2pgsql -s 3857 RoadNetworkMRWA_514.shp | psql -U postgres perth
-shp2pgsql -s 4283 LocalitiesLGATE_008.shp | psql -U postgres perth
-shp2pgsql -s 4283 LocalGovernmentAuthorityLGABoundariesLGATE_006.shp | psql -U postgres perth
+
+loadshape IntersectionsMRWA_510.zip 3857
+loadshape RoadNetworkMRWA_514.zip 3857
+loadshape LocalitiesLGATE_008.zip 4283
+loadshape LocalGovernmentAuthorityLGABoundariesLGATE_006.zip 4283
 

1b3f894881edd9a8e82b15f0af4af02b4e2c5ce3
Grahame Bowland <grahame@oreamnos.com.au>
2021-02-09 21:26:20 +0800

    use argparse() to handle CLI args

--- labyrinth/labyrinth/cli.py
+++ labyrinth/labyrinth/cli.py
@@ -1,10 +1,11 @@
 from .parishes import perth
 from .database import DatabaseAccess
+import argparse
 
 
-def main():
+def build(args):
     print("Building parishes:")
-    db = DatabaseAccess("postgres://postgres:postgres@localhost/perth")
+    db = DatabaseAccess(args.connection_string)
     for parish in perth:
         problems = getattr(parish, "problems", "")
         if problems and problems != "":
@@ -15,6 +16,21 @@ def main():
             )
         parish(db).generate()
     print("{} parishes generated.".format(len(perth)))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--connection-string", default="postgres://postgres:postgres@localhost/perth"
+    )
+    subparsers = parser.add_subparsers()
+
+    parse_build = subparsers.add_parser("build")
+    parse_build.add_argument("--geojson", type=str, required=False)
+    parse_build.set_defaults(func=build)
+
+    args = parser.parse_args()
+    args.func(args)
 
 
 if __name__ == "__main__":

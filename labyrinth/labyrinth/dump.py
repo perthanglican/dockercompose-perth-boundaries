68e3767b249f7b235e4a0610b44cd4de6aaae31e
Grahame Bowland <grahame@oreamnos.com.au>
2021-02-12 21:56:17 +0800

    multi-line output

--- labyrinth/labyrinth/dump.py
+++ labyrinth/labyrinth/dump.py
@@ -6,20 +6,23 @@ def to_json(db):
     session = db.session()
 
     def geom_to_geojson(geom):
-        return session.query(func.ST_ASGeoJSON(geom)).one()
+        return session.query(func.ST_ASGeoJSON(geom)).one()[0]
 
     def make_parish(p):
+        def to_lines(s):
+            return [t.strip() for t in s.split("\n")]
+
         parish = {
             "code": p.code,
             "name": p.name,
-            "definition": p.definition,
-            "problems": p.problems,
+            "definition": to_lines(p.definition),
+            "problems": to_lines(p.problems),
             "geom": geom_to_geojson(p.geom),
         }
         return parish
 
-    parishes = {}
+    parishes = []
     for result in session.query(Result).all():
-        parishes[result.name] = make_parish(result)
+        parishes.append(make_parish(result))
 
-    return parishes
+    return {"parishes": parishes}
